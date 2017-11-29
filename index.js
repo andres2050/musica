9b8eeaa6d748e51209521864f382bb1db72659c6
Andres Felipe Herrera Moreno <andres.2050@hotmail.com>
2017-11-28 22:21:28 -0500

    se completan todos los requerimientos necesarios
    -se agrega un campo para ingresar la ruta
    -se agrega glod para examinar todos los archivos mp3 de la ruta

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 var fpcalc = require("fpcalc");
 var request = require('request-promise');
+var glob = require("glob");
+var readline = require('readline');
 require('console.table');
 
 
 var idClient = "DZQCRv06EA";
 var apiUrl = "https://api.acoustid.org/v2/lookup";
 var musica = [];
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
+
+pedirInformacion();
 
 function buscarCanciones(canciones) {
     if(canciones.length === 0){
+        pedirInformacion();
         return;
     }
+    //se genera el fingerprint de las canciones
     fpcalc(canciones[0], function (err, result) {
         if (err) throw err;
+        //se genera una peticion post al servicio web
         request.post(
             {
                 uri: apiUrl,
@@ -35,6 +46,7 @@ function buscarCanciones(canciones) {
                             }
                             Artistas += body.results[i].recordings[j].artists[k].name;
                         }
+                        //se acamoda la informacion para obtener todos los resultados en caso de ser mas de uno
                         musica.push(
                             {
                                 Cancion: body.results[i].recordings[j].title,
@@ -49,15 +61,35 @@ function buscarCanciones(canciones) {
                 throw error;
             })
             .finally(function () {
+                //cuando termina se revisa si es la ultima cancion o si hay mas en la lista
                 if(canciones.length === 1){
                     console.table(musica);
+                    pedirInformacion();
                 }else{
-                    buscarCanciones(canciones.shift());
+                    //en casode haber mas se le pasa el resto de la lista
+                    canciones.shift();
+                    buscarCanciones(canciones);
                 }
 
-            })
+            });
     });
 }
 
+function examinarDirectorio(directorio) {
+    //examina todos los archivos mp3 del directorio dado
+    glob(directorio + "/*.mp3", function (er, files) {
+        buscarCanciones(files);
+    })
+}
 
-buscarCanciones(["./Cull.mp3"]);
+function pedirInformacion() {
+    //se pide la ruta en la que se desea ver la informacion, solo se han probado las rutas de linux
+    rl.question("Ingrese el directorio de sus archivos .mp3 \n", function (ruta) {
+        if (ruta === ""){
+            pedirInformacion()
+        }else{
+            console.log("Este proceso tardara dependiendo de la cantidad de archivos que tenga en la ruta.");
+            examinarDirectorio(ruta);
+        }
+    })
+}
